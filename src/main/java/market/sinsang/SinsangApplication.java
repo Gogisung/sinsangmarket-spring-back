586b5a896c27b30f4bd2cea353cb3ae1d1266d27
Gogisung <ggs0707@naver.com>
2020-10-20 18:24:34 +0900

    sinsang first

---
+++ src/main/java/market/sinsang/SinsangApplication.java
@@ -0,0 +1,13 @@
+package market.sinsang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinsangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/market/sinsang/SinsangApplicationTests.java
@@ -0,0 +1,13 @@
+package market.sinsang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinsangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
